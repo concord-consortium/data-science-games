b907f2170bc2dbc2771e144113c124bd7a54bd37
Tim Erickson <customer@eeps.com>
2015-10-06 10:49:48 -0700

    Add a bunch of documentation

--- gamePrototypes/geiger/geigerScript.js
+++ gamePrototypes/geiger/geigerScript.js
@@ -8,7 +8,9 @@ var gameCaseID = 0;
 var gameNumber = 0;
 var max_dose = 20000;
 
-
+/**
+ * called from the html when user presses the new game button
+ */
 function newGame() {
     if (gameCaseID > 0) {
         codapHelper.closeCase(
@@ -18,7 +20,9 @@ function newGame() {
         );
     }
     gameNumber += 1;
-
+    /**
+     * start a new game case
+     */
     codapHelper.openCase(
         'games',
         [this.gameNumber, 0, null, null],
@@ -29,18 +33,43 @@ function newGame() {
     console.log("New game. Source at (" + (geigerGameModel.sourceX) + ", " + (geigerGameModel.sourceY) + ")");
 }
 
+/**
+ * Used to set the game's caseID. Callback from codapHelper.openCase().
+ * @param iResult
+ */
 function    setUpNewGameData(iResult) {
     gameCaseID = iResult.caseID;
     console.log("got case ID " + (gameCaseID));
 }
 
+/**
+ * Singleton view of the "Lab" -- the region in which the speck might hide
+ *
+ * @type {{canvas: null, ctx: null, pixelsPerUnit: null, unitsAcross: number, setup: Function, update: Function, drawDetector: Function}}
+ */
 geigerLabView = {
+    /**
+     * The HTML <canvas>
+     *     @property
+     */
     canvas: null,
+    /**
+     * The drawing context for this.canvas
+     */
     ctx: null,
+    /**
+     * The scale of the coordinate system in the view; determined in this.setup()
+     */
     pixelsPerUnit: null,
+    /**
+     * How many units across is this canvas?
+     */
     unitsAcross: 100.0,
 
-
+    /**
+     * Sets up the properties
+     * Also adds event listeners
+     */
     setup: function() {
         this.canvas = document.getElementById("lab");
         this.canvas.addEventListener("mouseup",clickInLab,false);
@@ -51,6 +80,9 @@ geigerLabView = {
         ];
     },
 
+    /**
+     * Update this view.
+     */
     update: function() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -67,6 +99,9 @@ geigerLabView = {
         // window.requestAnimationFrame(geigerLabView.update);
     },
 
+    /**
+     * Draws the detector at the current location. Called by this.update().
+     */
     drawDetector: function( ) {
         var tContext = this.ctx;
 
@@ -90,15 +125,37 @@ geigerLabView = {
 
 };
 
+/**
+ * Model for the game.
+ * @type {{sourceX: number, sourceY: number, sourceStrength: number, detectorX: number, detectorY: number, dose: number, latestCount: number, setup: Function, signalStrength: Function, doMeasurement: Function}}
+ */
 geigerGameModel = {
+    /**
+     * (secret) position of the source of radiation
+     */
     sourceX: 0,
     sourceY: 0,
+    /**
+     * Strength of the radiation
+     */
     sourceStrength: 0,
+    /**
+     * Current position of the detector
+     */
     detectorX: 0,
     detectorY: 0,
+    /**
+     * Total dose so far
+     */
     dose: 0,
+    /**
+     * The radiation count for the last time the detector was used
+     */
     latestCount: 0,
 
+    /**
+     * Initialize model varaibles
+     */
     setup: function () {
         this.sourceX = (geigerLabView.unitsAcross * (0.25 + 0.50 * Math.random())).toFixed(2);
         this.sourceY = (geigerLabView.unitsAcross * (0.25 + 0.50 * Math.random())).toFixed(2); // TODO: fix vertical coordinate of source
@@ -106,6 +163,10 @@ geigerGameModel = {
         this.dose = 0;
     },
 
+    /**
+     * Compute the strength of the radiation at the detector. Use inverse square.
+     * @returns {number}
+     */
     signalStrength: function () {
         var tDsquared = (this.detectorX - this.sourceX ) * (this.detectorX - this.sourceX )
             + (this.detectorY - this.sourceY ) * (this.detectorY - this.sourceY );
@@ -114,7 +175,12 @@ geigerGameModel = {
         return tCount;
     },
 
-
+    /**
+     * Perform a measurement. Updates internal positions. Updates this.latestcount.
+     * TODO: figure out if we need to update the positions here. If not, we can even lose the parameters.
+     * @param x     X-position of detector.
+     * @param y
+     */
     doMeasurement: function( x, y ) {
         this.detectorX = x;
         this.detectorY = y;
@@ -127,14 +193,25 @@ geigerGameModel = {
 
 };
 
+/**
+ * Overall controller for the game.
+ *
+ * @type {{initializeComponent: Function, newGame: Function, updateScreen: Function, moveDetectorTo: Function, moveDetectorByTyping: Function, doMeasurement: Function}}
+ */
 geigerManager = {
 
+    /**
+     * Initializes various items.
+     */
     initializeComponent: function() {
         geigerLabView.setup();
         gauge.setup('doseGauge','Dose',0,max_dose);
         newGame();
     },
 
+    /**
+     * Resets Geiger for a new game.
+     */
     newGame: function() {
         displayInfo("New game. Find the source!");
         geigerGameModel.setup();
@@ -142,16 +219,27 @@ geigerManager = {
         this.updateScreen();
     },
 
+    /**
+     * Calls what's necessary to refresh the screen.
+     * This includes the Lab, the gauge, and any other stuff such as text.
+     */
     updateScreen: function() {
         geigerLabView.update();
         gauge.update( geigerGameModel.dose);
     },
 
+    /**
+     * In charge of moving the detector.
+     * Responds to clicks OR to edits in the coordinates from text boxes.
+     * @param x
+     * @param y
+     */
     moveDetectorTo: function (x, y) {
         geigerGameModel.detectorX = x;
         geigerGameModel.detectorY = y;
 
-        // make sure the coordinate boxes read the right values
+        // Make sure the coordinate boxes read the right values.
+        // Redundant if user has changed position by typing.
         document.getElementById('detectorX').value = x.toString();
         document.getElementById('detectorY').value = y.toString();
 
@@ -159,6 +247,10 @@ geigerManager = {
         this.updateScreen();
     },
 
+    /**
+     * Called when a coordinate text box loses focus (onblur).
+     * Updates coordinates of the detector.
+     */
     moveDetectorByTyping: function() {
         var x = document.forms.geigerForm.detectorX.value.trim();
         var y = document.forms.geigerForm.detectorY.value.trim();
@@ -170,6 +262,11 @@ geigerManager = {
         this.moveDetectorTo(x, y);
     },
 
+    /**
+     * User has called for a measurement.
+     * Creates a "measurement" case in CODAP.
+     * //TODO: determine if we really need to get the coordinates from the text fields
+     */
     doMeasurement: function() {
         var tX = document.forms.geigerForm.detectorX.value.trim();
         var tY = document.forms.geigerForm.detectorY.value.trim();
@@ -188,6 +285,9 @@ geigerManager = {
     }
 };
 
+/**
+ * Required call to initialize the sim, connect it to CODAP.
+ */
 codapHelper.initSim({
     name: 'Geiger',
     dimensions: {width: 444, height: 600},
@@ -219,6 +319,10 @@ codapHelper.initSim({
     ]
 });
 
+/**
+ * Called from HTML whn user clicks in the "Lab"
+ * @param e
+ */
 function clickInLab( e ) {
 //    Note: this routine gives page coordinates. We want the coordinates in the canvas.
     if (!e) e = window.event;
@@ -228,12 +332,19 @@ function clickInLab( e ) {
     geigerManager.moveDetectorTo(tX, tY);
 }
 
-
+/**
+ * Display function: basic info about the sim.
+ * @param message {string} The string message.
+ */
 function    displayInfo( message ) {
     var geigerInfoText = document.getElementById('geigerInfo');
     geigerInfoText.innerHTML = message;
 }
 
+/**
+ * Display function: the latest count from the geiger counter
+ * @param count The number to display
+ */
 function    displayGeigerCount(count) {
     var geigerCountText = document.getElementById('geigerCount');
     geigerCountText.innerHTML = "count: " + (count) + " at (" +
@@ -242,6 +353,12 @@ function    displayGeigerCount(count) {
 
 }
 
+/**
+ * A funky random Poisson function.
+ * Use Knuth algorithm up to n = 100; normal approximation beyond that.
+ * @param mean
+ * @returns {number}
+ */
 function    randomPoisson( mean ) {
 
     if (mean > 100) {
@@ -259,6 +376,12 @@ function    randomPoisson( mean ) {
     return (k - 1);
 }
 
+/**
+ * Random normal, Box-Muller transform. Use only one value.
+ * @param mean
+ * @param sd
+ * @returns {*}
+ */
 function    randomNormal(mean,sd) {
     var t1 = Math.random();
     var t2 = Math.random();
