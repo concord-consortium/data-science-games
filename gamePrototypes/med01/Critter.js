45d8fd7d9ae8dbebbd01ce4b5cfd4c81b659939d
Tim Erickson <customer@eeps.com>
2015-10-20 08:28:33 -0700

    Initial commit of med skeleton

---
+++ gamePrototypes/med01/Critter.js
@@ -0,0 +1,40 @@
+/*
+ ==========================================================================
+ Critter.js
+
+ Critter class for the med DSG.
+
+ Author:   Tim Erickson
+
+ Copyright (c) 2015 by The Concord Consortium, Inc. All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ ==========================================================================
+ */
+/**
+ * Created by tim on 10/19/15.
+ */
+
+
+var Critter = function() {
+    var x = 0;
+    var y = 0;
+    var destX = 0;
+    var destY = 0;
+};
+
+Critter.prototype.update = function (dt) {
+
+};
+
+
