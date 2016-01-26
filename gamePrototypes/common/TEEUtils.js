0d622243ed6acf7f560360f5b34492c401fda70c
Tim Erickson <customer@eeps.com>
2016-01-25 17:07:00 -0800

    Common files. And the Snap folder?

--- gamePrototypes/common/TEEUtils.js
+++ gamePrototypes/common/TEEUtils.js
@@ -25,45 +25,54 @@
  * Created by tim on 10/20/15.
  */
 
-/**
- * A funky random Poisson function.
- * Use Knuth algorithm up to n = 100; normal approximation beyond that.
- * @param mean
- * @returns {number}
- */
-function    randomPoisson( mean ) {
 
-    if (mean > 100) {
-        var sd = Math.sqrt(mean);
-        return Math.round(randomNormal(mean,sd));   //  todo: use randomNormal from common
+var     TEEUtils = {
+    /**
+     * A funky random Poisson function.
+     * Use Knuth algorithm up to n = 100; normal approximation beyond that.
+     * @param mean
+     * @returns {number}
+     */
+    randomPoisson : function(mean) {
+
+        if (mean > 100) {
+            var sd = Math.sqrt(mean);
+            return Math.round(randomNormal(mean, sd));   //  todo: use randomNormal from common
+        }
+        var L = Math.exp(-mean);
+        var p = 1.0;
+        var k = 0;
+        do {
+            k++;
+            p *= Math.random();
+        } while (p > L);
+        return (k - 1);
+    },
+
+    /**
+     * Random normal, Box-Muller transform. Use only one value.
+     * @param mean
+     * @param sd
+     * @returns {*}
+     */
+    randomNormal : function(mean, sd) {
+        var t1 = Math.random();
+        var t2 = Math.random();
+
+        var tZ = Math.sqrt(-2 * Math.log(t1)) * Math.cos(2 * Math.PI * t2);
+
+        return mean + sd * tZ;
+    },
+
+    /**
+     * Returns a random item from a list
+     * @param a         the list
+     * @returns {*}
+     */
+    pickRandomItemFrom : function(a) {
+        var tL = a.length;
+        var tR = Math.floor(Math.random() * tL);
+        return a[tR];
     }
-    var L = Math.exp(-mean);
-    var p = 1.0;
-    var k = 0;
-    do {
-        k++;
-        p *= Math.random();
-    } while (p > L);
-    return (k - 1);
-}
 
-/**
- * Random normal, Box-Muller transform. Use only one value.
- * @param mean
- * @param sd
- * @returns {*}
- */
-function    randomNormal(mean,sd) {
-    var t1 = Math.random();
-    var t2 = Math.random();
-
-    var tZ = Math.sqrt(-2 * Math.log(t1)) * Math.cos(2 * Math.PI*t2);
-
-    return mean + sd * tZ;
-}
-
-function    pickRandomItemFrom( a ) {
-    var tL = a.length;
-    var tR = Math.floor(Math.random() * tL);
-    return a[tR];
 }
