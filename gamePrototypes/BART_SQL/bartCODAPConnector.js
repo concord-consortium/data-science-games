40a6961eb059a16beec813a763b960d1fc5c19dc
Tim Erickson <customer@eeps.com>
2016-03-01 15:29:21 -0800

    BART 002a. More small changes.

--- gamePrototypes/BART_SQL/bartCODAPConnector.js
+++ gamePrototypes/BART_SQL/bartCODAPConnector.js
@@ -130,7 +130,7 @@ bartCODAPConnector.getInitSimObject = function() {
     var oInitSimObject = {
         name: 'BART',
         version : bartManager.version,
-        dimensions: {width: 500, height: 300},
+        dimensions: {width: 500, height: 256},
         collections: [  // There are two collections: a parent and a child
             {
                 name: 'games',
